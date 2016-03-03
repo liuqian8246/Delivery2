d7665850685ef6344f166ae6b2ae5336fed0a57e
liuqian <594176665@qq.com>
2016-03-03 19:58:02 +0800

    Delivery2

---
+++ ExpressDelivery/app/src/main/java/com/tboys/expressdelivery/activity/QueryDeliveryActivity.java
@@ -0,0 +1,34 @@
+package com.tboys.expressdelivery.activity;
+
+import android.support.design.widget.NavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.tboys.expressdelivery.R;
+
+public class QueryDeliveryActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_query_delivery);
+        toolbar = (Toolbar) findViewById(R.id.toolbar_query);
+        setSupportActionBar(toolbar);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+    }
+}
